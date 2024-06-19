1db14dc6462f2d9c99e02de1e480452e371a5ff4
mayonaka <shinnya1024@gmail.com>
2024-06-19 12:54:24 +0900

    fix ファイル名禁止文字をすべてエスケープするようにした

--- RJRenemer.ps1
+++ RJRenemer.ps1
@@ -1,43 +1,61 @@
 Import-Module AngleParse
 
-# パラメータ配列をパス別に再構築.
-$files_args = New-Object System.Collections.ArrayList
-foreach($arg in $Args){
-    if([System.IO.Path]::IsPathRooted($arg)){
-        $files_args.Add($arg) | Out-Null
-    }
-    else{
-        # ファイルパスの先頭でない文字列は前の文字列の後にスペースで結合.
-        $files_args[$files_args.Count-1] = $files_args[$files_args.Count-1] + " " + $arg
-    }
+# ファイル名禁止文字全角置換くん
+Function replaceForbiddenChar($fileName) {
+  $invalidChar = @{'"' = '”'; '<' = '＜'; '>' = '＞'; '|' = '｜'; '*' = '＊'; '?' = '？'; '\' = '￥'; '/' = '／'; }
+  foreach ($c in $invalidChar.GetEnumerator()) {
+    # Write-Output $c
+    $fileName = $fileName.Replace($c.Key, $c.Value)
+  }
+  # Write-Output $fileName
+  return $fileName
 }
 
-foreach($file_path in $files_args){
+
+# パラメータ配列をパス別に再構築.
+$files_args = New-Object System.Collections.ArrayList
+foreach ($arg in $Args) {
+  if ([System.IO.Path]::IsPathRooted($arg)) {
+    $files_args.Add($arg) | Out-Null
+  }
+  else {
+    # ファイルパスの先頭でない文字列は前の文字列の後にスペースで結合.
+    $files_args[$files_args.Count - 1] = $files_args[$files_args.Count - 1] + " " + $arg
+  }
+}
+
+foreach ($file_path in $files_args) {
   Write-Output $file_path
   $RJNumber = (Get-Item $file_path).BaseName
   Write-Output ( $RJNumber)
   $circleNameSelector = "#work_maker > tbody > tr > td > span > a"
   $titleSelector = "#work_name"
-  $typeSelector= "#category_type > a > span"
-  $deteilSelector= "#work_outline > tbody > tr"
+  $typeSelector = "#category_type > a > span"
+  $deteilSelector = "#work_outline > tbody > tr"
   $DlSitePage = Invoke-WebRequest "https://www.dlsite.com/maniax/work/=/product_id/$RJNumber"
   $circleName = $DlSitePage | Select-HtmlContent $circleNameSelector
   $title = $DlSitePage | Select-HtmlContent $titleSelector
   $type = $DlSitePage | Select-HtmlContent $typeSelector
   if ($type -eq "ボイス・ASMR") {
-    $CV= $DlSitePage | Select-HtmlContent $deteilSelector
-    $parsedCV = ((($CV -match "声優") -replace "声優","")-replace "\s","")-replace "/","・"
+    Write-Output("Type:Voice/ASMR")
+    $CV = $DlSitePage | Select-HtmlContent $deteilSelector
+    $parsedCV = ((($CV -match "声優") -replace "声優", "") -replace "\s", "")
     $newFileName = "[$circleName($parsedCV)]$title.zip"
-  } else {
+  }
+  else {
+    Write-Output("Type:Other")
     $author = $DlSitePage | Select-HtmlContent $deteilSelector
-    $parsedAuthor = ((($author -match "作者") -replace "作者","")-replace "\s","")-replace "/","・"
-    Write-Output ($author -match "作者")
-    if($parsedAuthor -eq ""){
-      $parsedAuthor = ((($author -match "イラスト") -replace "イラスト","")-replace "\s","")-replace "/","・"
+    $parsedAuthor = ((($author -match "作者") -replace "作者", "") -replace "\s", "")
+    Write-Output([bool]$parsedAuthor)
+    if (![bool]$parsedAuthor) {
+      Write-Output("GetIllust")
+      $parsedAuthor = ((($author -match "イラスト") -replace "イラスト", "") -replace "\s", "")
+      Write-Output($parsedAuthor)
     }
     $newFileName = "[$circleName($parsedAuthor)]$title.zip"
   }
 
-  Write-Output $newFileName
-  Rename-Item $file_path $newFileName
+  $escapedNewFileName = replaceForbiddenChar($newFileName)
+  Write-Output $escapedNewFileName
+  Rename-Item $file_path $escapedNewFileName
 }
